c5539c8b0f676881bb8ba7e4ff8768d172fe8022
AnishSherchan <sherchananish11@gmail.com>
2023-08-18 11:11:42 +0545

    Unused Varibale Removed

--- src/components/SimpleModal/index.js
+++ src/components/SimpleModal/index.js
@@ -34,7 +34,6 @@ const SimpleModal = ({ isOpen, closeModal, title, modalData }) => {
     } else {
       reset();
     }
-    // esling disable
     /* eslint-disable-next-line */
   }, [modalData]);
 
